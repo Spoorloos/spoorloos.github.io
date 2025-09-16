86902f032bc7044c3f5cefed1c69327e401d1c69
Spoorloos <mick.negenman@icloud.com>
2025-09-16 15:26:02 +0200

    Changed site url

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://spoorloos.github.io/",
+    site: "https://www.spoorloos.xyz/",
     vite: {
         plugins: [
             tailwindcss()
