201f299a8bb3a5f659e61180e7bd59d96a872a52
Spoorloos <mick.negenman@icloud.com>
2025-10-08 15:29:36 +0200

    Added languages

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,30 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import path from "node:path";
+import fs from "node:fs";
+
+const srcFolder = path.resolve(import.meta.dirname, "./src/");
+const localesFolder = path.resolve(srcFolder, "./locales/");
+const localeNames = fs.readdirSync(localesFolder).map((lang) => path.parse(lang).name);
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://www.spoorloos.xyz/",
     vite: {
         plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                "@": srcFolder,
+            },
+        },
     },
     integrations: [sitemap()],
+    i18n: {
+        locales: localeNames,
+        defaultLocale: "en",
+        routing: {
+            prefixDefaultLocale: true,
+        },
+    },
 });
